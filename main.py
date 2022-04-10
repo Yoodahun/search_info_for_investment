0a08b70f3379e079ef489c9d168cd72409a8a203
Yoodahun <dahun4032@gmail.com>
2022-04-10 21:42:09 +0900

    UPDATE refactoring

--- main.py
+++ main.py
@@ -1,51 +1,42 @@
 import datetime
 import time
+
+import filter_data
 from extract_data.extract import Extract
 from export_data import ExportToData
-from extract_data.basic_factor_data import korean_market_factor_data
+
 
 """
 condition = {
-    'PBR': 1.0,
-    'PER': 8,
+    'PBR': 0.8,
+    'PER': 10,
     'DIV': 5.0
 }
 """
 
-
-# test1 = korean_market_factor_data.KoreanMarketFactorData()
-
-# test1.get_kospi_market_data()
-# test1.get_etf_market_data()
-# test2 = extract.low_pbr_and_per("KOSPI")
-# print(test2.head(2))
-
-# pd.set_option('display.max_columns', None)
-# test = stock.get_market_fundamental_by_date("20220317", "20220318", "005930")
-# test = test.reset_index()
-# print(test)
 #
 
 
 start = time.time()
 extractor = Extract()
 exporter = ExportToData()
-# test3 = extractor.filter_high_div_and_dps("KOSPI")
 kospi_kosdaq_data = extractor.get_data()
 print(kospi_kosdaq_data)
-"""
-condition = {
-    'PBR': 1.0,
-    'PER': 8,
-    'DIV': 5.0
-}
-"""
-LOW_PBR_AND_PER = extractor.filter_low_pbr_and_per(0.8, 8, kospi_kosdaq_data)
+extracted_data = extractor.extract_finance_data(kospi_kosdaq_data)
+
 # KOSDAQ_LOW_PBR_AND_PER = extractor.filter_low_pbr_and_per(1.0, 10, kosdaq_data)
 
+# exporter.export_to_excel("/Users/yoodahun/Documents/Dahun Document/Investment information/total.xlsx", kospi_kosdaq_data)
+exporter.export_to_excel("/Users/yoodahun/Documents/Dahun Document/Investment information/filtered.xlsx",
+                         filter_data.filtering_data_that_market_cap_under_thirty_percent(kospi_kosdaq_data)
+                         )
+
+
 exporter.export_to_excel_with_many_sheets(
-    "/Users/yoodahun/Documents/Dahun Document/Investment information/test5_excel_file.xlsx",
-    [kospi_kosdaq_data]
+    "/Users/yoodahun/Documents/Dahun Document/Investment information/screeningData.xlsx",
+    [
+        filter_data.filtering_low_pbr_and_per(0.8,10, extracted_data)
+    ]
 )
 
 end = time.time()
@@ -54,7 +45,3 @@ sec = (end - start)
 result_list = str(datetime.timedelta(seconds=sec)).split(".")
 print(f"Total extracting time : {result_list[0]} ---------------------")
 
-# print(test3.head(10))
-
-# dt = datetime.datetime.now()
-# print(dt.strftime("%Y%m%d"))
