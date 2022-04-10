6f2645d1847f6a3c8e0dfad3ea260781f8f39420
Yoodahun <dahun4032@gmail.com>
2022-04-10 18:11:59 +0900

    UPDATE exporter

--- export_data/export_to_excel.py
+++ export_data/export_to_excel.py
@@ -6,12 +6,20 @@ class ExportToData:
     def __init__(self):
         self.pandas = pd
 
+    def export_to_excel(self, file_path, file):
+        print("Exporting result to excel file.....")
+        writer = self.pandas.ExcelWriter(file_path, engine='openpyxl')
+        file.to_excel(writer, sheet_name="test")
+
+        writer.save()
+
+
     # TODO export to excel
     def export_to_excel_with_many_sheets(self, file_path, files: array):
         print("Exporting result to excel file.....")
         writer = self.pandas.ExcelWriter(file_path, engine='openpyxl')
 
-        files[0].to_excel(writer, sheet_name="KOSPI_저PER_저PBR")
-        files[1].to_excel(writer, sheet_name="KOSDAQ_저PER_저PBR")
+        files[0].to_excel(writer, sheet_name="저PBR_저PER")
+        # files[1].to_excel(writer, sheet_name="KOSDAQ_저PER_저PBR")
 
         writer.save()
