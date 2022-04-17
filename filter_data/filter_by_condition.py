38ef7d054df3565d36e1ff69f20b02f1fbc634e2
Yoodahun <dahun4032@gmail.com>
2022-04-17 13:12:56 +0900

    UPDATE filter condition

--- filter_data/filter_by_condition.py
+++ filter_data/filter_by_condition.py
@@ -31,13 +31,22 @@ def filtering_data_that_market_cap_under_thirty_percent(data: pd.DataFrame):
         data[data["거래량"] == 0].index,
         inplace=True
     )
-    return data[data["시가총액"] <= data["시가총액"].quantile(q=0.4)].sort_values(by=["시가총액"], ascending=True)
+    return data[data["시가총액"] <= data["시가총액"].quantile(q=0.3)].sort_values(by=["시가총액"], ascending=True)
 
 
 def filtering_low_pbr_and_per(pbr: float, per: float, df: pd.DataFrame):
-    pbr_condition = df['PBR'] <= pbr
-    per_condition = df['PER'] > 0 | df['PER'] <= per
-    df = df[pbr_condition & per_condition]
+    pbr_condition_1 = df['PBR'] >= 0.2
+    pbr_condition_2 = df['PBR'] <= pbr
+    per_condition_1 = df['PER'] > 0
+    per_condition_2 = df['PER'] <= per
+
+    df.drop(
+        df[ df["연도"] != "2021-12-30"].index,
+        inplace=True
+    )
+
+    df = df[ (pbr_condition_1 & pbr_condition_2)]
+    df = df[(per_condition_1 & per_condition_2) ]
 
     return ("LOW_PBR_AND_PER",
             df.sort_values(by=['PER', 'PBR', '종목코드', '연도'], ascending=[True, True, False, False]).reset_index(drop=True)
@@ -45,23 +54,38 @@ def filtering_low_pbr_and_per(pbr: float, per: float, df: pd.DataFrame):
 
 
 def filtering_low_pbr_and_high_gpa(pbr: float, df: pd.DataFrame):
-    pbr_condition = df['PBR'] <= pbr
+    pbr_condition_1 = df['PBR'] >= 0.2
+    pbr_condition_2 = df['PBR'] <= pbr
+    per_condition_1 = df['PER'] > 0
     # gpa_condition = df['GP/A'].quantile(q=gpa)
 
-    df = df[pbr_condition]
+    df.drop(
+        df[ df["연도"] != "2021-12-30"].index,
+        inplace=True
+    )
+
+    df = df[ (pbr_condition_1 & pbr_condition_2) & per_condition_1 ]
 
     return ("LOW_PBR_AND_HIGH_GPA",
-            df.sort_values(by=['PBR', 'GP/A', '종목명', '연도'], ascending=[True, False, False, False]).reset_index(drop=True)
+            df.sort_values(by=['PBR', 'GP/A', '종목명', '연도'], ascending=[True, False, False, False]).reset_index(
+                drop=True)
             )
 
 
-def filtering_high_ncav_cap(ncav: float, df: pd.DataFrame):
-    ncav_condition = df['NCAV/MK'] >= ncav
+def filtering_high_ncav_cap_and_gpa(df: pd.DataFrame):
     net_income_condition = df['당기순이익'] > 0
+    liabilities_condition = df['부채비율'] < 200.0
+    ncav_condition = df['NCAV/MK'] > 0.0
 
-    df = df[ncav_condition & net_income_condition]
-    return ("HIGH_NCAV/CAP",
-            df.sort_values(by=['NCAV/MK', '종목명', '연도'], ascending=[False, False, False]).reset_index(drop=True)
+    df.drop(
+        df[ df["연도"] != "2021-12-30"].index,
+        inplace=True
+    )
+
+    df = df[ncav_condition & net_income_condition & liabilities_condition]
+    return ("고NCAV_GPA_저부채비율",
+            df.sort_values(by=['NCAV/MK', 'GP/A', '종목명', '연도'], ascending=[False, False, False, False]).reset_index(
+                drop=True)
             )
 
 
@@ -71,6 +95,11 @@ def filtering_value_factor(df: pd.DataFrame):
     :param df:
     :return:
     """
+    df.drop(
+        df[ df["연도"] != "2021-12-30"].index,
+        inplace=True
+    )
+
     df["PBR rank"] = df["PBR"].rank(ascending=True)
     df["PER rank"] = df["PER"].rank(ascending=True)
     df["PCR rank"] = df["PCR"].rank(ascending=True)
@@ -79,6 +108,6 @@ def filtering_value_factor(df: pd.DataFrame):
     df["4 Total Value score"] = df["PBR rank"] + df["PER rank"] + df["PCR rank"] + df["PSR rank"]
 
     return ("HIGH_SCORE_Four_value",
-            df.sort_values(by=['4 Totla Value score', '종목명', '연도'], ascending=[True, False, False]).reset_index(
-        drop=True)
+            df.sort_values(by=['4 Total Value score', '종목명', '연도'], ascending=[True, False, False]).reset_index(
+                drop=True)
             )
