70c0168dfb0993a15bb9984c1b288160744a763d
Yoodahun <dahun4032@gmail.com>
2022-11-26 00:00:23 +0900

    ADD export excel condition

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ exporter.export_to_excel_with_many_sheets(
     [
         filter_data.filtering_low_per("ALL_DATA_저PER", kospi_kosdaq_data.copy(), True),
         filter_data.filtering_low_pbr_and_per("ALL_DATA_저PBR_저PER", 1.0, 10, kospi_kosdaq_data.copy(), True),
+        filter_data.filtering_s_rim_disparity_all_data("S-RIM ALL_DATA", kospi_kosdaq_data.copy()),
         filter_data.filtering_low_per("소형주_저PER", extracted_data.copy()),
         filter_data.filtering_low_pbr_and_per("소형주_저PBR_저PER", 1.0, 10, extracted_data.copy()),
         filter_data.filtering_low_psr_and_per("소형주_저PSR_저PER", 10, extracted_data.copy()),
@@ -35,7 +36,8 @@ exporter.export_to_excel_with_many_sheets(
         filter_data.filtering_high_propensity_to_dividend("소형주 고배당성향", extracted_data.copy()),
         filter_data.filtering_low_pfcr("소형주_저PFCR_시총잉여현금흐름", extracted_data.copy()),
         filter_data.filtering_low_pbr_and_high_gpa("소형주_저PBR_고GPA", 0.8, extracted_data.copy()),
-        filter_data.filtering_high_ncav_cap_and_gpa("소형주_저NCAV_GPA_저부채비율", extracted_data.copy()),
+        filter_data.filtering_high_ncav_cap_and_gpa("소형주_고NCAV_GPA_저부채비율", extracted_data.copy()),
+        filter_data.filtering_s_rim_disparity_and_high_nav("소형주_S-RIM_괴리율_고NAV", extracted_data.copy()),
         filter_data.filtering_profit_momentum("소형주_모멘텀_전분기대비_영업이익순이익_전략", extracted_data.copy()),
         filter_data.filtering_value_factor("소형주_슈퍼가치_4가지_전략", extracted_data.copy()),
         filter_data.filtering_value_and_profit_momentum("소형주_성장주모멘텀_전략", extracted_data.copy()),
@@ -43,7 +45,7 @@ exporter.export_to_excel_with_many_sheets(
         filter_data.filtering_value_factor2("소형주_12가지_팩터순위합계", extracted_data.copy()),
         filter_data.filtering_value_factor_upgrade("소형주_강환국_슈퍼가치전략_업글", extracted_data.copy()),
         # filter_data.filtering_value_and_quality("소형주_저PBR_고GPA_자산성장률계산", 1.0, extracted_data.copy()),
-        filter_data.filtering_new_F_score_and_low_pbr("소형주_NEW F Score and Low PBR", extracted_data.copy()),
+        # filter_data.filtering_new_F_score_and_low_pbr("소형주_NEW F Score and Low PBR", extracted_data.copy()),
 
         ("Extracted_RAW_Data", extracted_data),
         ("RAW_Data", kospi_kosdaq_data)
